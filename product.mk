be0e21b117f2bfd23686522b3d378d622593b1a3
HDzungx <hdzungx@gmail.com>
2026-09-01 02:22:37 +0700

    extra: Add Google Sans font
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- product.mk
+++ product.mk
@@ -21,3 +21,9 @@ PRODUCT_PACKAGES += \
 # RRO to hide NexusLauncher gesture hint  
 PRODUCT_PACKAGES += \
     PixelLauncherNoGestureHintOverlay
+
+# Fonts
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/extra/fonts/ttf,$(TARGET_COPY_OUT_PRODUCT)/fonts)
+
+PRODUCT_COPY_FILES += \
+    vendor/extra/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
